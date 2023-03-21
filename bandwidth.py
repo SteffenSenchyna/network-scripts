dc2bbe34cf8d7782a5f36e5ae0eaef49dc4996fa
SteffenSenchyna <105737839+SteffenSenchyna@users.noreply.github.com>
2023-03-20 23:13:04 -0600

    Create bandwidth.py

---
+++ bandwidth.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+import pytz
+import time
+import json
+from pymongo import MongoClient
+import speedtest
+# To login into mongo container shell
+# docker exec -it mongodb bash
+# mongosh
+# use network
+# db.bandwidth.find()
+# db.bandwidth.deleteMany({})
+client = MongoClient('mongodb://localhost:27017/')
+networkDB = client.network
+bandwidthTable = networkDB.bandwidth
+speed_test = speedtest.Speedtest()
+counter = 0
+
+
+def bytes_to_mb(bytes):
+    KB = 1024  # One Kilobyte is 1024 bytes
+    MB = KB * 1024  # One MB is 1024 KB
+    return int(bytes/MB)
+
+
+try:
+    while (counter < 1800):
+        download_speed = bytes_to_mb(speed_test.download())
+        upload_speed = bytes_to_mb(speed_test.upload())
+        utc_now = pytz.utc.localize(
+            datetime.utcnow()).strftime("%Y-%m-%d:%H-%M-%S")
+        bandwidth = {
+            "downloadSpeed": download_speed,
+            "uploadSpeed":  upload_speed,
+            "created_at": datetime.utcnow(),
+        }
+        result = bandwidthTable.insert_one(bandwidth)
+        counter += 30
+        time.sleep(30)
+except Exception as e:
+    print(e)
